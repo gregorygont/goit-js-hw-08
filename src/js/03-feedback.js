0d1e8bcc269e691107a39587f26b127ced3bd662
Grygorii Gontarchuk <105290753+gregorygont@users.noreply.github.com>
2023-05-29 12:47:53 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -31,7 +31,7 @@ function onInputData(e) {
 // Функция onInputData вызывается при событии input на любом поле формы. 
 // Она обновляет объект dataForm текущими значениями полей email и message и сохраняет обновленный объект 
 // в локальное хранилище с помощью метода localStorage.setItem().
-onInputData();
+
 function reloadPage() {
   if (dataForm) {
     email.value = dataForm.email || '';
@@ -53,7 +53,7 @@ function onFormSubmit(e) {
   e.currentTarget.reset();
   dataForm = {};
 }
-onFormSubmit();
+
 //Функция onFormSubmit вызывается при отправке формы. Она предотвращает стандартное поведение отправки формы, 
 //выводит значения email и message в консоль. Затем она проверяет, заполнены ли оба поля. 
 //Если хотя бы одно поле не заполнено, выводится предупреждение с помощью функции alert. 
